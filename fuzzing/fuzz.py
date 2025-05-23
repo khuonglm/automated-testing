6ef016634e3aba36e7a921d21b45345baf1963cf
khuonglm <lemanhkhuong2611@gmail.com>
2025-05-24 00:12:51 +0900

    add utils

---
+++ fuzzing/fuzz.py
@@ -0,0 +1,8 @@
+from utils import RandomGenerator
+
+class APISequencer:
+    def __init__(self, graph: dict[str, list[dict[str, str]]]):
+        self.graph = graph
+
+    def sequence(self):
+        pass
---
+++ fuzzing/utils.py
@@ -0,0 +1,83 @@
+import random
+import string
+
+class RandomGenerator:
+    STR_CHARS = "!@#$%^&*" + string.ascii_lowercase + string.digits + string.ascii_uppercase
+    STR_CHARS_NO_SPECIAL = string.ascii_lowercase + string.digits + string.ascii_uppercase
+    STR_CHARS_NO_UPPER = string.ascii_lowercase + string.digits + "!@#$%^&*"
+    STR_CHARS_NO_LOWER = string.ascii_uppercase + string.digits + "!@#$%^&*"
+    STR_CHARS_NO_DIGITS = string.ascii_lowercase + string.ascii_uppercase + "!@#$%^&*"
+    STR_CHARS_NO_SPECIAL_NO_UPPER = string.ascii_lowercase + string.digits
+    STR_CHARS_NO_SPECIAL_NO_LOWER = string.ascii_uppercase + string.digits
+    STR_CHARS_NO_SPECIAL_NO_DIGITS = string.ascii_lowercase + string.ascii_uppercase
+    INT_CHARS = string.digits
+    BOOL_CHARS = ["true", "false"]
+    MIN_LENGTH = 1
+    MAX_LENGTH = 100
+
+    def __init__(self):
+        pass
+
+    def generate_random_length(self) -> int:
+        return random.randint(self.MIN_LENGTH, self.MAX_LENGTH)
+
+    def generate_random_bool(self) -> bool:
+        return random.choice(self.BOOL_CHARS)
+
+    def generate_random_string(self, length: int | None = None, chars: str = STR_CHARS) -> str:
+        if length is None:
+            length = self.generate_random_length()
+        return ''.join(random.choice(chars) for _ in range(length))
+
+    def generate_random_int(self, length: int | None = None, chars: str = INT_CHARS) -> int:
+        if length is None:
+            length = self.generate_random_length()
+        return int(''.join(random.choice(chars) for _ in range(length)))
+
+    def generate_random_float(self, length: int | None = None, chars: str = INT_CHARS) -> float:
+        if length is None:
+            length = self.generate_random_length()
+        point = random.randint(1, length - 1)
+        return float(''.join(random.choice(chars) for _ in range(point)) + '.' + ''.join(random.choice(chars) for _ in range(length - point)))
+
+    def generate_random_list(self, length: int | None = None, obj: any = None) -> list[any]:
+        value = []
+        if length is None:
+            length = self.generate_random_length()
+        for _ in range(length):
+            if isinstance(obj, str):
+                value.append(self.generate_random_string(None, self.STR_CHARS_NO_SPECIAL))
+            elif isinstance(obj, int):
+                value.append(self.generate_random_int(None, self.INT_CHARS))
+            elif isinstance(obj, float):
+                value.append(self.generate_random_float(None, self.INT_CHARS))
+            elif isinstance(obj, list):
+                value.append(self.generate_random_list(None, obj[0]))
+            elif isinstance(obj, dict):
+                value.append(self.generate_random_dict(obj))
+        return value
+
+    def generate_random_dict(self, obj: dict[str, any]) -> dict[str, any]:
+        value = {}
+        for key, value in obj.items():
+            if isinstance(value, str):
+                value[key] = self.generate_random_string(None, self.STR_CHARS_NO_SPECIAL)
+            elif isinstance(value, int):
+                value[key] = self.generate_random_int(None, self.INT_CHARS)
+            elif isinstance(value, float):
+                value[key] = self.generate_random_float(None, self.INT_CHARS)
+            elif isinstance(value, list):
+                value[key] = self.generate_random_list(None, value[0])
+            elif isinstance(value, dict):
+                value[key] = self.generate_random_dict(value)
+            elif isinstance(value, bool):
+                value[key] = self.generate_random_bool()
+        return value
+
+    def sample_from_list(self, length: int | None = None, population: list[any] = None) -> any:
+        """
+        sample a list of unique random numbers from a population
+        """
+        if length is None:
+            length = self.generate_random_length()
+        return random.sample(population, length)
