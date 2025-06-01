c4055f9468a6d0ef482766e0fb5ee33a0df354c2
Cashew <tungbinem@gmail.com>
2025-06-01 21:50:20 +0900

    wip: api fuzzing

--- fuzzing/fuzz.py
+++ fuzzing/fuzz.py
@@ -1,6 +1,91 @@
-from fuzzing.utils.random_generator import RandomGenerator
-from fuzzing.utils.adaptive_random_generator import AdaptiveRandomGenerator
-from typing import Literal
+from utils.random_generator import RandomGenerator
+from utils.adaptive_random_generator import AdaptiveRandomGenerator
+from typing import Literal, Any
+import random
+import requests
+from os import path
+import re
+
+example_apis = [
+    {
+        "method": "POST",
+        "url": "/api/users/login",
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "path_parameters": {},
+        "query_parameters": {},
+        "body": {
+            "user": {
+                "email": "<string>",
+                "password": "<string>"
+            }
+        }
+    },
+    {
+        "method": "POST",
+        "url": "/api/users",
+        "headers": {},
+        "path_parameters": {},
+        "query_parameters": {},
+        "body": {
+            "user": {
+                "username": "<string>",
+                "email": "<string>",
+                "password": "<string>"
+            }
+        }
+    },
+    {
+        "method": "GET",
+        "url": "/api/user",
+        "headers": {
+            "Authorization": "Token <token>"
+        },
+        "path_parameters": {},
+        "query_parameters": {},
+        "body": {}
+    },
+    {
+        "method": "PUT",
+        "url": "/api/user",
+        "headers": {
+            "Authorization": "Token <token>"
+        },
+        "path_parameters": {},
+        "query_parameters": {},
+        "body": {
+            "user": {
+                "username": "<string>",
+                "email": "<string>",
+                "bio": "<string>",
+                "password": "<string>"
+            }
+        }
+    },
+    {
+        "method": "GET",
+        "url": "/api/profiles/<username>",
+        "headers": {},
+        "path_parameters": {
+            "username": "<string>"
+        },
+        "query_parameters": {},
+        "body": {}
+    },
+    {
+        "method": "POST",
+        "url": "/api/profiles/<username>/follow",
+        "headers": {
+            "Authorization": "Token <token>"
+        },
+        "path_parameters": {
+            "username": "<string>"
+        },
+        "query_parameters": {},
+        "body": {}
+    },
+]
 
 example_graph = {
    "0": [
@@ -34,8 +119,9 @@ example_graph = {
 }
 
 class APISequencer:
-    def __init__(self, graph: dict[str, list[dict[str, str]]]):
+    def __init__(self, graph: dict[str, list[dict[str, str]]], apis: list[Any]):
         self.graph = graph
+        self.apis = apis
 
     # Helper method for Depth-first Search
     def _dfs(self, node: str, visited: set[str], stack: list[str]):
@@ -77,21 +163,76 @@ class APISequencer:
         
         # Reverse so that dependent APIs are called last
         return stack[::-1]
+    
+    # def random(self) -> list[str]:
+    #     apis = [str(x) for x in range(len(self.apis))]
+    #     random.shuffle(apis)
+    #     return apis
 
-    def sequence(self, method: Literal["dfs"] | Literal["bfs"] ="dfs"):
-        if method == "dfs":
-            ordered_sequence = self.dfs()
-        elif method == "bfs":
-            ordered_sequence = self.bfs()
-        else:
-            raise TypeError("method should be 'dfs' or 'bfs'.")
+    def sequence(self, method: Literal["dfs"] | Literal["bfs"] = "dfs"):
+        match method:
+            case "dfs":
+                ordered_sequence = self.dfs()
+            case "bfs":
+                ordered_sequence = self.bfs()
+            # case "random":
+            #     ordered_sequence = self.random()
+            case _:
+                raise TypeError("invalid method.")
 
-        print("call order: ", ordered_sequence)
+        # print("call order: ", ordered_sequence)
         return ordered_sequence
 
+class Fuzzer:
+    def __init__(self, apis):
+        self.apis = apis
+
+    def fuzz_api(api, random_generator = RandomGenerator()):
+        if api["method"] not in ["GET", "POST", "PUT", "DELETE", "PATCH"]:
+            raise ValueError("Invalid method {method}!")  
+        method = api["method"].lower()
+        path_params_schema = api["path_parameters"]
+        query_params_schema = api["query_parameters"]
+        url_template = api["url"]
+
+        while True:
+            url = url_template
+            # Populate path parameters
+            for param in path_params_schema:
+                param_type = path_params_schema[param]
+                match param_type:
+                    case "<string>":
+                        url = url.replace("<{param}>", random_generator.generate_random_string())
+                    case "<integer>":
+                        url = url.replace("<{param}>", random_generator.generate_random_int())
+            
+            # Populate query parameters
+            query_params = {}
+            for param in query_params_schema:
+                param_type = path_params_schema[param]
+                match param_type:
+                    case "<string>":
+                        query_params[param] = random_generator.generate_random_string()
+                    case "<integer>":
+                        query_params[param] = random_generator.generate_random_int()
+            
+                
+            pass
+
+    def call_api(self, method: str, url: str, headers, body, baseurl=""):
+        url = path.join(baseurl, url)
+        request = getattr(requests, method)
+        return request(url, headers=headers, data=body)
+
+    def fuzz_sequence(self, sequence: list[str]):
+        sequence = [int(idx) for idx in sequence]
+        for idx in sequence:
+            self.fuzz_api(self.apis[idx])
+
 if __name__ == '__main__':
-    print("hello world")
-    sequencer = APISequencer(example_graph)
-    sequencer.sequence("bfs")
+    sequencer = APISequencer(example_graph, example_apis)
+    sequencer.sequence("dfs")
+    fuzzer = Fuzzer(example_apis)
+    print(fuzzer.call_api(example_apis[0], "http://localhost:5000"))
 
     
